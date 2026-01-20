451cfb7ae5ddd4984bad952b2eb19ad0f74c8a9b
shlok728 <shloksingh612@gmail.com>
2026-01-20 23:45:03 +0530

    Add files via upload

---
+++ src/pages/Home.jsx
@@ -0,0 +1,75 @@
+import MovieCard from "../components/MovieCard";
+import { useState, useEffect } from "react";
+import { SearchMovies,getPopularMovies } from "../services/api";
+import "../css/Home.css"
+
+
+function Home(){
+    const [searchQuery, setSearchQuery] = useState("");
+
+    const [movies, setMovies] = useState([]);
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(true);
+    
+    
+
+    useEffect(() => {
+        const loadPopularMovies = async () => {
+            try{
+                const popularMovies = await getPopularMovies();
+                setMovies(popularMovies);
+            } catch (err) {
+                console.error(err);
+                setError("Failed to fetch popular movies.");
+            }
+            finally{
+                setLoading(false);
+            }
+        }
+        loadPopularMovies();
+    }, []);
+
+    const handleSearch=async (e) => {
+        e.preventDefault();
+        if (!searchQuery.trim()) return
+        if (loading) return;
+        setLoading(true);
+        try {
+            const searchResults = await SearchMovies(searchQuery);
+            setMovies(searchResults);
+            setError(null);
+
+
+        } catch (err) {
+            console.log(err);
+            setError("Failed to search");
+
+        } finally {
+            setLoading(false);
+        }
+
+    
+    }
+
+    return (
+        <div className="home">
+            <form onSubmit={handleSearch} className="search-form">
+            <input type="text" placeholder="Search movies..." className="search-input" value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)}/>
+            <button type="submit" className="search-button">Search</button>
+
+            </form>
+            {error && <div className="error">{error}</div>}
+        {loading ? (
+        <div className="loading">Loading...</div>
+    ) : (
+
+        <div className="movie-grid">
+           {movies.map((Movie) => (
+                < MovieCard movie={Movie} key={Movie.id}/>
+            ))}
+        </div>
+  )}
+    </div>
+    );
+}
+export default Home
--- src/pages/favorites.jsx
+++ src/pages/favorites.jsx
@@ -1,1 +1,28 @@
+import "../css/Favorites.css"
+import { useMovieContext } from "../context/MovieContext";
+import MovieCard from "../components/Moviecard";
 
+
+function Favorites() {
+    const { favorites } = useMovieContext();
+    if (favorites) {
+        return(
+        <div className="favorites">
+        <h2>Your Favorite Movies</h2>
+        <div className="movie-grid">
+           {favorites.map((Movie) => (
+                < MovieCard movie={Movie} key={Movie.id}/>
+            ))}
+        </div>
+        </div>
+        )
+
+    }
+    return <div className="favorites-empty">
+        <h2>No Favorite Movies yet</h2>
+        <p>Start adding movies to your favorites and they will appear here.</p>
+    </div>
+    ;
+}
+
+export default Favorites;
