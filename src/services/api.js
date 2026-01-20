9589299d905ffb5e450ad6931a9c2858de27ddff
shlok728 <shloksingh612@gmail.com>
2026-01-20 23:47:22 +0530

    Add files via upload

--- src/services/api.js
+++ src/services/api.js
@@ -1,1 +1,14 @@
+const API_KEY = "886f7969f10495c0f3d035ce5067ec32"
+const BASE_URL = "https://api.themoviedb.org/3"
 
+export const getPopularMovies = async () => {
+    const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+    const data = await response.json();
+    return data.results;
+}
+
+export const SearchMovies = async (query) => {
+    const response = await fetch(`${BASE_URL}/search/movie?api_key=${API_KEY}&query=${query}&query={encodeURIComponent(query)}`);
+    const data = await response.json();
+    return data.results;
+}
